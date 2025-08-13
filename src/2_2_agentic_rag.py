182dd6d62dad52ab9db9e0398c3a8416c6a5bc58
wrhlearner <ronghaowang24@gmail.com>
2025-08-13 14:18:14 +0800

    add script 2.2 agentic RAG

--- src/2_2_agentic_rag.py
+++ src/2_2_agentic_rag.py
@@ -1,1 +1,344 @@
 # reference: https://langchain-ai.github.io/langgraph/tutorials/rag/langgraph_agentic_rag/#5-rewrite-question
+# reference: langchain ollama embeddings https://python.langchain.com/docs/integrations/text_embedding/ollama/
+
+# setup environment
+from email import message
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# preprocessing documents
+from langchain_community.document_loaders import WebBaseLoader
+
+urls = [
+    "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
+    "https://lilianweng.github.io/posts/2024-07-07-hallucination/",
+    "https://lilianweng.github.io/posts/2024-04-12-diffusion-video/",
+]
+
+# solve below error
+# urllib3.exceptions.MaxRetryError: HTTPSConnectionPool(host='lilianweng.github.io', port=443): 
+#     Max retries exceeded with url: /posts/2024-11-28-reward-hacking/ 
+#     (Caused by SSLError(SSLEOFError(8, '[SSL: UNEXPECTED_EOF_WHILE_READING] EOF occurred in violation of protocol (_ssl.c:1028)')))
+loader = WebBaseLoader(urls)
+loader.requests_per_second = 1
+docs = loader.aload()
+
+# print(docs[0][0].page_content.strip()[:1000])
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# docs_list = [item for sublist in docs for item in sublist]
+docs_list = [item for item in docs]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=50, chunk_overlap=25
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# create a retriever tool
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_ollama import OllamaEmbeddings
+
+embeddings = OllamaEmbeddings(
+    model=os.environ["MODEL"],
+)
+
+vectorstore = InMemoryVectorStore.from_documents(
+    documents=doc_splits, 
+    embedding=embeddings
+)
+retriever = vectorstore.as_retriever()
+
+from langchain.tools.retriever import create_retriever_tool
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and return information about Lilian Weng blog posts.",
+)
+
+# results = retriever_tool.invoke({"query": "types of reward hacking"})
+
+# from pprint import pprint
+# pprint(results)
+
+# generate query
+from langgraph.graph import MessagesState
+from langchain_ollama import ChatOllama
+
+response_model = ChatOllama(
+    model=os.environ["MODEL"],
+    temperature=0,
+)
+
+def generate_query_or_respond(state: MessagesState):
+    """Call the model to generate a response based on the current state. Given
+    the question, it will decide to retrieve using the retriever tool, or simply respond to the user.
+    """
+    response = (
+        response_model
+        .bind_tools([retriever_tool]).invoke(state["messages"])
+    )
+    return {"messages": [response]}
+
+# input = {"messages": [{"role": "user", "content": "Hello!"}]}
+# generate_query_or_respond(input)["messages"][-1].pretty_print()
+
+# input = {
+#     "messages": [
+#         {
+#             "role": "user",
+#             "content": "What does Lilian Weng say about types of reward hacking?",
+#         }
+#     ]
+# }
+# generate_query_or_respond(input)["messages"][-1].pretty_print()
+
+# grade documents: whether the retrieved documents are relevant
+from pydantic import BaseModel, Field
+from typing import Literal
+
+GRADE_PROMPT = (
+    "You are a grader assessing relevance of a retrieved document to a user question. \n "
+    "Here is the retrieved document: \n\n {context} \n\n"
+    "Here is the user question: {question} \n"
+    "If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n"
+    "Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."
+)
+
+
+class GradeDocuments(BaseModel):
+    """Grade documents using a binary score for relevance check."""
+
+    binary_score: str = Field(
+        description="Relevance score: 'yes' if relevant, or 'no' if not relevant"
+    )
+
+
+grader_model = ChatOllama(
+    model=os.environ["MODEL"],
+    temperature=0,
+)
+
+
+def grade_documents(
+    state: MessagesState,
+) -> Literal["generate_answer", "rewrite_question"]:
+    """Determine whether the retrieved documents are relevant to the question."""
+    question = state["messages"][0].content
+    context = state["messages"][-1].content
+
+    prompt = GRADE_PROMPT.format(question=question, context=context)
+    response = (
+        grader_model
+        .with_structured_output(GradeDocuments).invoke(
+            [{"role": "user", "content": prompt}]
+        )
+    )
+    score = response.binary_score
+
+    if score == "yes":
+        return "generate_answer"
+    else:
+        return "rewrite_question"
+
+from langchain_core.messages import convert_to_messages
+
+# input = {
+#     "messages": convert_to_messages(
+#         [
+#             {
+#                 "role": "user",
+#                 "content": "What does Lilian Weng say about types of reward hacking?",
+#             },
+#             {
+#                 "role": "assistant",
+#                 "content": "",
+#                 "tool_calls": [
+#                     {
+#                         "id": "1",
+#                         "name": "retrieve_blog_posts",
+#                         "args": {"query": "types of reward hacking"},
+#                     }
+#                 ],
+#             },
+#             {"role": "tool", "content": "meow", "tool_call_id": "1"},
+#         ]
+#     )
+# }
+# results = grade_documents(input)
+# for i in range(len(input["messages"])):
+#     print(f"Message: {input["messages"][i].content}\n\n\n")
+# print(results)
+
+
+# rewrite questions
+REWRITE_PROMPT = (
+    "Look at the input and try to reason about the underlying semantic intent / meaning.\n"
+    "Here is the initial question:"
+    "\n ------- \n"
+    "{question}"
+    "\n ------- \n"
+    "Formulate an improved question:"
+)
+
+
+def rewrite_question(state: MessagesState):
+    """Rewrite the original user question."""
+    messages = state["messages"]
+    question = messages[0].content
+    prompt = REWRITE_PROMPT.format(question=question)
+    response = response_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": convert_to_messages([{"role": "user", "content": response.content}])}
+
+# input = {
+#     "messages": convert_to_messages(
+#         [
+#             {
+#                 "role": "user",
+#                 "content": "What does Lilian Weng say about types of reward hacking?",
+#             },
+#             {
+#                 "role": "assistant",
+#                 "content": "",
+#                 "tool_calls": [
+#                     {
+#                         "id": "1",
+#                         "name": "retrieve_blog_posts",
+#                         "args": {"query": "types of reward hacking"},
+#                     }
+#                 ],
+#             },
+#             {"role": "tool", "content": "meow", "tool_call_id": "1"},
+#         ]
+#     )
+# }
+
+# response = rewrite_question(input)
+# print(response["messages"][-1]["content"])
+
+
+# generate an answer
+GENERATE_PROMPT = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer the question. "
+    "If you don't know the answer, just say that you don't know. "
+    "Use three sentences maximum and keep the answer concise.\n"
+    "Question: {question} \n"
+    "Context: {context}"
+)
+
+
+def generate_answer(state: MessagesState):
+    """Generate an answer."""
+    question = state["messages"][0].content
+    context = state["messages"][-1].content
+    prompt = GENERATE_PROMPT.format(question=question, context=context)
+    response = response_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": [response]}
+
+# input = {
+#     "messages": convert_to_messages(
+#         [
+#             {
+#                 "role": "user",
+#                 "content": "What does Lilian Weng say about types of reward hacking?",
+#             },
+#             {
+#                 "role": "assistant",
+#                 "content": "",
+#                 "tool_calls": [
+#                     {
+#                         "id": "1",
+#                         "name": "retrieve_blog_posts",
+#                         "args": {"query": "types of reward hacking"},
+#                     }
+#                 ],
+#             },
+#             {
+#                 "role": "tool",
+#                 "content": "reward hacking can be categorized into two types: environment or goal misspecification, and reward tampering",
+#                 "tool_call_id": "1",
+#             },
+#         ]
+#     )
+# }
+
+# response = generate_answer(input)
+# response["messages"][-1].pretty_print()
+
+
+# assemble the graph
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import tools_condition
+
+workflow = StateGraph(MessagesState)
+
+# Define the nodes we will cycle between
+workflow.add_node(generate_query_or_respond)
+workflow.add_node("retrieve", ToolNode([retriever_tool]))
+workflow.add_node(rewrite_question)
+workflow.add_node(generate_answer)
+
+workflow.add_edge(START, "generate_query_or_respond")
+
+# Decide whether to retrieve
+workflow.add_conditional_edges(
+    "generate_query_or_respond",
+    # Assess LLM decision (call `retriever_tool` tool or respond to the user)
+    tools_condition,
+    {
+        # Translate the condition outputs to nodes in our graph
+        "tools": "retrieve",
+        END: END,
+    },
+)
+
+# Edges taken after the `action` node is called.
+workflow.add_conditional_edges(
+    "retrieve",
+    # Assess agent decision
+    grade_documents,
+)
+workflow.add_edge("generate_answer", END)
+workflow.add_edge("rewrite_question", "generate_query_or_respond")
+
+# Compile
+graph = workflow.compile()
+
+# visualize the graph
+try:
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    outputFilename = os.path.join(current_path, '..', 'tests', '2_2_agentic_rag.png')
+    if not os.path.exists(outputFilename):
+        graph.get_graph().draw_mermaid_png(output_file_path=outputFilename)
+        print(f"Saved graph to {outputFilename}")
+    else:
+        print(f"{outputFilename} already exists")
+except Exception:
+    # This requires some extra dependencies and is optional
+    pass
+
+# run the agentic RAG
+# in this example, the model reaches max recursion limit
+# this is because at the later stages, the output of rewrite_question is always same as the last output, thus fall in an infinite loop
+# to solve this problem, need to check whether a new rewritten question is generated compared to histories, and whether new documents are retrieved
+for chunk in graph.stream(
+    {
+        "messages": [
+            {
+                "role": "user",
+                "content": "What does Lilian Weng say about types of reward hacking?",
+            }
+        ]
+    }
+):
+    for node, update in chunk.items():
+        print("Update from node", node)
+        update["messages"][-1].pretty_print()
+        print("\n\n")
+
+
