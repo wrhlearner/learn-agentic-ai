ba17575bfd24f140e8aedfa28baeecad95cf3ca7
wrhlearner <ronghaowang24@gmail.com>
2025-08-15 15:31:26 +0800

    add script place holders

---
+++ src/4_7_ragas_LLM_app_evaluation_with_custom_metrics.py
@@ -0,0 +1,1 @@
+# reference: https://docs.ragas.io/en/stable/howtos/customizations/metrics/_write_your_own_metric_advanced/
---
+++ src/6_a2a_langgraph_tutorial.py
@@ -0,0 +1,1 @@
+# reference: https://github.com/a2aproject/a2a-samples/tree/main/samples/python/agents/langgraph
